3d9456713aa75f38237f1407309208aa1fd47f57
crocanoq <68356969+crocanoq@users.noreply.github.com>
2020-07-15 18:40:50 -0500

    Add files via upload

---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+
+    {
+        ServiceReference1.Service1Client cliente = new ServiceReference1.Service1Client();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                int n = Convert.ToInt32(textBox1.Text);
+                label2.Text = cliente.operation(n).ToString();
+                        
+
+            }
+            catch(FaultException<ServiceReference1.ArgumentFault> aex)
+            {
+                label2.Text = "Msj Argument: " + aex.Detail.Message;
+                
+
+            }
+            catch (FaultException<ServiceReference1.InvalidOperationFault> iox)
+            {
+                label2.Text = "Msj InvalidOperation: " + iox.Detail.Message;
+            }
+            catch (Exception ex)
+            {
+                label2.Text = "Error desconocido: " + ex.Message;
+            }
+
+        }
+    }
+}
---
+++ WindowsFormsApp3/Form2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form2 : Form
+    {
+        
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            ServiceReference1.Service1Client cliente = new ServiceReference1.Service1Client();
+            MessageBox.Show(cliente.intCounter().ToString());
+            MessageBox.Show(cliente.intCounter().ToString());
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            ServiceReference2.Service2Client cliente1 = new ServiceReference2.Service2Client();
+            MessageBox.Show(cliente1.intCountersession().ToString());
+           // MessageBox.Show(cliente1.intCountersession().ToString());           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ServiceReference3.Service3Client cliente2 = new ServiceReference3.Service3Client();
+            //contador++;
+            MessageBox.Show(cliente2.contadorSingle().ToString());
+        }
+    }
+}
